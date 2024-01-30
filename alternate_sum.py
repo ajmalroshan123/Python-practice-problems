3c68d0f23373dc51b9026535e5da6483e3fc7020
ajmalroshan123 <119650990+ajmalroshan123@users.noreply.github.com>
2024-01-30 21:36:04 +0530

    Add files via upload

---
+++ alternate_sum.py
@@ -0,0 +1,14 @@
+arr = []
+n = int(input("Enter the size :"))
+for i in range(0,n):
+    element = int(input("Enter the Element no. {} :".format(i+1)))
+    arr.append(element)
+size = len(arr)
+for i in range(size):
+    print('{}'.format(arr[i]))
+print("The alternate sum. ")
+print('The original list :'+str(arr))
+res = [sum(arr[i : : 2]) for i in range(len(arr)//len(arr)//2)]   
+#Print(result)
+print("The alternate element summation list :"+ str(arr))
+print('\n'd)
